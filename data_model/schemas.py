de41f3c1f8c91da0ea01fb0f6928eec9d41b4437
Cccryer <cyrcryy@gmail.com>
2025-04-05 19:11:15 +0800

    add create_final_documents

---
+++ data_model/schemas.py
@@ -0,0 +1,17 @@
+ID = "id"
+SHORT_ID = "human_readable_id"
+TITLE = "title"
+TEXT = "text"
+TEXT_UNIT_IDS = "text_unit_ids"
+CREATION_DATE = "creation_date"
+METADATA = "metadata"
+
+DOCUMENTS_FINAL_COLUMNS = [
+    ID,
+    SHORT_ID,
+    TITLE,
+    TEXT,
+    TEXT_UNIT_IDS,
+    CREATION_DATE,
+    METADATA,
+]
